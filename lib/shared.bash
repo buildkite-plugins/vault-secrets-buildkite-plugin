928ca32c8026e51682e5edfefcf2ea938e324a0c
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2022-09-12 17:29:27 -0600

    WIP: first attempt at using the new Vault CLI with AppRole auth and KVv2 Secrets Engine

--- lib/shared.bash
+++ lib/shared.bash
@@ -12,26 +12,56 @@ esac
 vault_auth() {
   local server="$1"
   local auth_params=''
-  # role is defined in Vault Configuration
-  # -header=<foo> # used to set X-Auth
-  # BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD - aws
-  # BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_HEADER
-  # BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE
+
+  # Currently we only support AppRole authentication.
+  # These values are referenced when authenticating to the Vault server:
+  #   BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD - approle
+
+  #   RoleID and SecretID should be stored securely on the agent, there are probably better ways to do this, but here is a start.
+
+  #   BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_SECRET_ID
+  #   BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_ROLE_ID
+
   [ -n "${server:-}" ] && auth_params="${auth_params} -address=${server}"
   [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD:-}" ] && auth_params="${auth_params} -method=${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD}"
-  [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_HEADER:-}" ] && auth_params="${auth_params} -header_value=${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_HEADER}"
-  [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE:-}" ] && auth_params="${auth_params} role=${BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE}"
 
-  if [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD:-}" ] ; then
-    # don't output the token to log, even though it's a temporary token
-    # shellcheck disable=SC2086
-    vault auth $auth_params | grep -v ^token:
+  if [ "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD}" = "approle" ]; then
+
+    [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE_ID:-}" ] && auth_params="${auth_params} role_id=${BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE_ID}"
+    [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_SECRET_ID:-}" ] && auth_params="${auth_params} secret_id=${BUILDKITE_PLUGIN_VAULT_SECRETS_SECRET_ID}"
+
+    vault write -field=token auth/approle/login \
+    role_id="$BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE_ID" \
+    secrets_id= "$BUILDKITE_PLUGIN_VAULT_SECRETS_SECRET_ID"
     return "${PIPESTATUS[0]}"
-  else
-    # shellcheck disable=SC2086
-    echo "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_TOKEN:-}" | vault auth ${auth_params:-} -
   fi
+
 }
+
+## This is the old auth method for reference
+# vault_auth() {
+#   local server="$1"
+#   local auth_params=''
+#   # role is defined in Vault Configuration
+#   # -header=<foo> # used to set X-Auth
+#   # BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD - aws
+#   # BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_HEADER
+#   # BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE
+#   [ -n "${server:-}" ] && auth_params="${auth_params} -address=${server}"
+#   [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD:-}" ] && auth_params="${auth_params} -method=${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD}"
+#   [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_HEADER:-}" ] && auth_params="${auth_params} -header_value=${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_HEADER}"
+#   [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE:-}" ] && auth_params="${auth_params} role=${BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE}"
+
+#   if [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_METHOD:-}" ] ; then
+#     # don't output the token to log, even though it's a temporary token
+#     # shellcheck disable=SC2086
+#     vault auth $auth_params | grep -v ^token:
+#     return "${PIPESTATUS[0]}"
+#   else
+#     # shellcheck disable=SC2086
+#     echo "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_TOKEN:-}" | vault auth ${auth_params:-} -
+#   fi
+# }
 
 list_secrets() {
   local server="$1"
@@ -89,6 +119,7 @@ add_ssh_private_key_to_agent() {
   fi
 
   echo "Loading ssh-key into ssh-agent (pid ${SSH_AGENT_PID:-})" >&2;
+
   echo "$ssh_key" | env SSH_ASKPASS="/bin/false" ssh-add -
 }
 
