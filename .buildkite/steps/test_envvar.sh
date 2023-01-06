b78bb4eeed8774dac3537573ad17518d6a5b8b9d
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2023-01-06 15:52:17 -0700

    update integration tests to work with new version of vault

--- .buildkite/steps/test_envvar.sh
+++ .buildkite/steps/test_envvar.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 set -eu
 
 configEnv() {
-  _source="${BASH_SOURCE[0]}"
+  _source="$0"
   [ -z "${_source:-}" ] && _source="${0}"
   basedir="$( cd "$( dirname "${_source}" )" && cd ../.. && pwd )"
 }
 
 setupTestData() {
   export VAULT_TOKEN="${VAULT_DEV_ROOT_TOKEN_ID:-}"
-  ${basedir}/.buildkite/steps/vault-init-tester.sh
+  sh "${basedir}/.buildkite/steps/vault-init-tester.sh"
 }
 
 runTest() {
@@ -17,16 +18,17 @@ runTest() {
   export BUILDKITE_PIPELINE_SLUG="${BUILDKITE_PIPELINE_SLUG:-}"
   export BUILDKITE_PLUGIN_VAULT_SECRETS_SERVER="${BUILDKITE_PLUGIN_VAULT_SECRETS_SERVER:-}"
 
-  source ${basedir}/hooks/environment
+  . "${basedir}"/hooks/environment
 
-  if [[ ! -z "${TESTDATA_1:-}" ]] && [[ "${TESTDATA_1}" == "foo bar 1" ]] ; then
+
+  if [[ -n "${TESTDATA_1:-}" ]] && [[ "${TESTDATA_1}" == "foobar1" ]] ; then
     echo "TESTDATA_1 is correct"
   else
     echo "TESTDATA_1 is not set and/or correct"
     exit 1
   fi
 
-  if [[ ! -z "${TESTDATA_2:-}" ]] && [[ "${TESTDATA_2}" == "foo bar 2" ]] ; then
+  if [[ ! -z "${TESTDATA_2:-}" ]] && [[ "${TESTDATA_2}" == "foobar2" ]] ; then
     echo "TESTDATA_2 is correct"
   else
     echo "TESTDATA_2 is not set and/or correct"
@@ -34,9 +36,9 @@ runTest() {
   fi
 }
 
-cmd="$0"
+# cmd="$0"
 configEnv
-if [ -d /app ] ; then
+if [ -d /app ]; then
   echo "--- setup test data"
   setupTestData
   echo "--- run test"
--- .buildkite/steps/vault-init-tester.sh
+++ .buildkite/steps/vault-init-tester.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# This token is defined in docker-compoise.yml and is for testing ONLY!!!!!!
+# This token is defined in docker-compose.yml and is for testing ONLY!!!!!!
 AUTH_TOKEN="${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_TOKEN:-}"
 PROJECT=${BUILDKITE_PIPELINE_SLUG:-foobar_project}
-vault auth - <<< "$AUTH_TOKEN"
+export VAULT_ADDR="http://vault-svc:8200"
 
-[ $? -eq 0 ] && vault mount -path data/buildkite kv
 
-TESTDATA_1=`echo TESTDATA_1=\"foo bar 1\" | base64`
-TESTDATA_2=`echo TESTDATA_2=\"foo bar 2\" | base64`
+vault login token="$AUTH_TOKEN"
+
+[ $? -eq 0 ] && vault secrets enable -path=data/buildkite kv
+
+TESTDATA_1="foobar1"
+TESTDATA_2="foobar2"
 
 [ $? -ne 1 ] && {
-   echo "${TESTDATA_1}" | vault write data/buildkite/env/TESTDATA_1 value=-
-   echo "${TESTDATA_2}" | vault write data/buildkite/${PROJECT}/env/TESTDATA_2 value=-
+   vault kv put data/buildkite/env TESTDATA_1="${TESTDATA_1}"
+   vault kv put data/buildkite/"${PROJECT}"/env TESTDATA_2="${TESTDATA_2}"
 }
